48901b17f4ef9eb2389d566565b0a66940bdbf12
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2024-12-08 15:21:38 +0500

    Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+import BrowseTheRangeSection from "@/components/sections/BrowseTheRangeSection";
+import HeroSection from "@/components/sections/HeroSection";
+import OurProductSection from "@/components/sections/OurProductSection";
+import ShareSetupSection from "@/components/sections/ShareSetupSection";
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
-  )
+    <main className="bg-white">
+      <HeroSection />
+      <div className="flex flex-col gap-[56px] mx-4 md:mx-[130px]">
+        <BrowseTheRangeSection />
+        <OurProductSection />
+      </div>
+      <div className="mt-[56px]">
+        <ShareSetupSection />
+      </div>
+    </main>
+  );
 }

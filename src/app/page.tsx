03c52d6c7e9c9dd23117da9077f742fa3c6db85c
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2024-12-08 16:15:41 +0500

    Shop Router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import BrowseTheRangeSection from "@/components/sections/BrowseTheRangeSection";
+import SlickRoomSLider from "@/components/sections/Hero-Slider";
 import HeroSection from "@/components/sections/HeroSection";
 import OurProductSection from "@/components/sections/OurProductSection";
 import ShareSetupSection from "@/components/sections/ShareSetupSection";
@@ -10,6 +11,9 @@ export default function Home() {
       <div className="flex flex-col gap-[56px] mx-4 md:mx-[130px]">
         <BrowseTheRangeSection />
         <OurProductSection />
+      </div>
+      <div>
+        <SlickRoomSLider />
       </div>
       <div className="mt-[56px]">
         <ShareSetupSection />

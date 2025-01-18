f28e3b6c74e62838e4fbabb3b0343f8441dc996f
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-01-18 16:54:25 +0500

    Detailed Page Fixes

--- src/components/sections/shop/product-detail/ProductDetailRelatedSection.tsx
+++ src/components/sections/shop/product-detail/ProductDetailRelatedSection.tsx
@@ -1,92 +1,34 @@
+"use client";
 import ProductCard from "@/components/cards/ProductCard";
 import MainButton from "@/components/common/MainButton";
-import React from "react";
+import { client } from "@/sanity/lib/client";
+import { ImportedData } from "@/types";
+import React, { useEffect, useState } from "react";
 
 function ProductDetailRelatedSection() {
-  const data = [
-    {
-      id: "1",
-      imageUrl: "/images/p_1.png",
-      title: "Syltherine",
-      description: "Stylish cafe chair",
-      price: "2.500.000",
-      otherPrice: "3.500.000",
-      type: "DISCOUNTED",
-      typeValue: "-30%",
-    },
-    {
-      id: "2",
-      imageUrl: "/images/p_2.png",
-      title: "Leviosa",
-      description: "Stylish cafe chair",
-      price: "2.500.000",
-      otherPrice: "3.500.000",
-      type: "DISCOUNTED",
-      typeValue: "-30%",
-    },
-    {
-      id: "3",
-      imageUrl: "/images/p_3.png",
-      title: "Lolito",
-      description: "Luxury big sofa",
-      price: "7.000.000",
-      otherPrice: "14.000.000",
-      type: "DISCOUNTED",
-      typeValue: "-50%",
-    },
-    {
-      id: "4",
-      imageUrl: "/images/p_4.png",
-      title: "Respira",
-      description: "Outdoor bar table and stool",
-      price: "500.000",
-      otherPrice: "",
-      type: "NEW",
-      typeValue: "New",
-    },
-    {
-      id: "5",
-      imageUrl: "/images/p_5.png",
-      title: "Grifo",
-      description: "Night lamp",
-      price: "1.500.000",
-      otherPrice: "",
-      type: "NORMAL",
-      typeValue: "",
-    },
+  const [data, setData] = useState<ImportedData[]>([]);
+  const query = `*[_type == "product"]{
+        _id,
+        title,
+        "imageUrl": imageUrl.asset->url,
+        price,
+        tags,
+        description,
+        dicountPercentage,
+        isNew,  
+      }`
+  useEffect(() => {
+    const fetchDataFromSanity = async () => {
+      try {
+        const PRODUCTS = await client.fetch(query);
+        setData(PRODUCTS);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchDataFromSanity();
+  }, [])
 
-    {
-      id: "6",
-      imageUrl: "/images/p_6.png",
-      title: "Muggo",
-      description: "Small mug",
-      price: "150.000",
-      otherPrice: "",
-      type: "NEW",
-      typeValue: "New",
-    },
-    {
-      id: "7",
-      imageUrl: "/images/p_7.png",
-      title: "Pingky",
-      description: "Cute bed set",
-      price: "7.000.000",
-      otherPrice: "14.000.000",
-      type: "DISCOUNTED",
-      typeValue: "-50%",
-    },
-
-    {
-      id: "8",
-      imageUrl: "/images/p_8.png",
-      title: "Potty",
-      description: "Minimalist flower pot",
-      price: "500.000",
-      otherPrice: "",
-      type: "NEW",
-      typeValue: "New",
-    },
-  ];
   return (
     <section className="w-full overflow-x-hidden">
       <div>
@@ -94,7 +36,7 @@ function ProductDetailRelatedSection() {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-[20px] mt-[30px]">
         {data.map((item, index) => (
-          <ProductCard {...item} key={index} />
+          index < 8 && <ProductCard {...item} key={index} />
         ))}
       </div>
       <div className="flex justify-center mt-[32px]">

47cb40930f639368b53b2e58c27034a6b8d95da1
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-01-21 22:52:22 +0500

    Search On The Shop

--- src/components/sections/shop/ShopProductSection.tsx
+++ src/components/sections/shop/ShopProductSection.tsx
@@ -1,11 +1,13 @@
 "use client";
 import ProductCard from "@/components/cards/ProductCard";
+import { useSearch } from "@/context/searchContext";
 import { client } from "@/sanity/lib/client";
 import { ImportedData } from "@/types";
 import { query } from "@/utils/query";
 import { useEffect, useState } from "react";
 
 function ShopProductSection() {
+  const { searchQuery } = useSearch();
   const [PRODUCTS, setPRODUCTS] = useState<ImportedData[]>([]);
   useEffect(() => {
     const fetchDataFromSanity = async () => {
@@ -17,11 +19,16 @@ function ShopProductSection() {
       }
     }
     fetchDataFromSanity();
-  }, [])
+  }, []);
+
+  const filteredProducts = PRODUCTS.filter((product) =>
+    product.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    product.description.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   return (
     <section>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-[32px] mt-[46px]">
-        {PRODUCTS.map((item, index) => (
+        {filteredProducts.map((item, index) => (
           <ProductCard {...item} key={index} />
         ))}
       </div>

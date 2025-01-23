d95a5824a90c93d911d53945941e2b08a30f6977
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-01-23 14:29:06 +0500

    Filter Section Wrapped In Filter Provider

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -2,12 +2,15 @@ import ShopBannerSection from "@/components/sections/shop/ShopBannerSection";
 import ShopFilterSection from "@/components/sections/shop/ShopFilterSection";
 import ShopHeroSection from "@/components/sections/shop/ShopHeroSection";
 import ShopProductSection from "@/components/sections/shop/ShopProductSection";
+import { FilterProvider } from "@/context/filterContext";
 
 function ShopPage() {
     return (
         <div>
             <ShopHeroSection />
-            <ShopFilterSection />
+            <FilterProvider>
+                <ShopFilterSection />
+            </FilterProvider>
             <div className="mx-4 md:mx-[130px]">
                 <ShopProductSection />
             </div>

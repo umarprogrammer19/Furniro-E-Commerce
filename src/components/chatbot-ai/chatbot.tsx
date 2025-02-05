d50eddb6782ea5f2d40d5d886cdafc915a248895
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-05 23:10:24 +0500

    Chatbot Developed

---
+++ src/components/chatbot-ai/chatbot.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useEffect } from "react";
+
+const Chatbot = () => {
+    useEffect(() => {
+        const appId = process.env.NEXT_PUBLIC_KOMMUNICATE_APP_ID;
+
+        if (!appId) {
+            console.error("Kommunicate App ID is missing in environment variables!");
+            return;
+        }
+
+        const kommunicateSettings = {
+            appId,
+            popupWidget: true,
+            automaticChatOpenOnNavigation: true,
+        };
+
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.async = true;
+        script.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+
+        script.onload = () => {
+            (window as any).kommunicate = (window as any).kommunicate || {};
+            (window as any).kommunicate._globals = kommunicateSettings;
+        };
+
+        document.head.appendChild(script);
+
+        return () => {
+            document.head.removeChild(script);
+        };
+    }, []);
+
+    return null;
+};
+
+export default Chatbot;

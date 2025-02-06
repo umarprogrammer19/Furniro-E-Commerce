746dec7d7f1bde3f1bbef1634b2147ee5e1599aa
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-06 23:32:14 +0500

    CheckOut Responsive

--- src/components/sections/checkout/CheckoutDetailSection.tsx
+++ src/components/sections/checkout/CheckoutDetailSection.tsx
@@ -19,28 +19,30 @@ function CheckoutDetailSection() {
   const totalPrice = computeSubTotal();
 
   return (
-    <section className="w-1/2">
-      <div className="flex justify-between">
-        <p className="font-bold text-[18px]">Product</p>
-        <p className="font-bold text-[18px]">Subtotal</p>
+    <section className="w-full sm:w-1/2 mx-auto px-4">
+      <div className="flex justify-between text-sm sm:text-base">
+        <p className="font-bold">Product</p>
+        <p className="font-bold">Subtotal</p>
       </div>
 
       <div className="mt-4 flex flex-col gap-3 justify-between">
         {products.map((product, index) => (
-          <div key={index} className="flex justify-between">
-            <p className="text-customGray2 text-sm ">
+          <div key={index} className="flex justify-between items-center">
+            <p className="text-customGray2">
               {product.title}{" "}
               <span className="font-bold text-black">X {product.quantity}</span>
             </p>
-            <p>{Number(product.price) + " x " + Number(product.quantity)} = ${Number(product.price) * Number(product.quantity)}</p>
+            <p>
+              ${Number(product.price) * Number(product.quantity)} (${Number(product.price)} x {Number(product.quantity)})
+            </p>
           </div>
         ))}
       </div>
 
       <div className="mt-4 flex flex-col gap-3 justify-between">
-        <div className="flex justify-between">
-          <p className="text-customGray2 text-sm ">Total</p>
-          <p className="text-primary font-bold text-[20px]">${totalPrice}</p>
+        <div className="flex justify-between items-center">
+          <p className="text-customGray2 text-sm">Total</p>
+          <p className="text-primary font-bold text-lg sm:text-xl">${totalPrice}</p>
         </div>
       </div>
 
@@ -48,7 +50,7 @@ function CheckoutDetailSection() {
         <Separator />
       </div>
 
-      <p>
+      <p className="text-sm sm:text-base">
         Your personal data will be used to support your experience throughout
         this website, to manage access to your account, and for other purposes
         described in our <strong>privacy policy</strong>.

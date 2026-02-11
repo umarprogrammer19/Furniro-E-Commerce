eaa31d047cc2f9956288f9fca480dece998b0f0c
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-02-11 12:13:14 +0500

    middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
     try {
         const accessToken = req.cookies.get("accessToken")
         if (!accessToken) {

77b8d15d426073d1dc79ec498610383a91ab9446
umarprogrammer19 <umarprogrammer19666@gmail.com>
2026-03-21 23:51:26 +0500

    Added Comment

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -7,5 +7,5 @@ export const client = createClient({
   dataset,
   apiVersion,
   useCdn: true,
-  token: process.env.SECRET_API_TOKEN
+  token: process.env.SECRET_API_TOKEN // For Developer Options
 })
